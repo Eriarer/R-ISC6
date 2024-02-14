181ac09ec7e0699f929593d2989754d32c1fe9d0
Eri <103050349+Eriarer@users.noreply.github.com>
2024-02-14 16:22:37 -0600

    formatting

--- 01_Organizar/numeros_aleatorios.r
+++ 01_Organizar/numeros_aleatorios.r
@@ -1,25 +1,25 @@
 # Bubble Sort
-bubble_sort <- function (x,ascending = TRUE) {
-   n <- length(x)
-   if (ascending) {
-     for(i in 1:(n-1)){
-       for(j in 1:(n-i)) {
-         if(x[j+1] < x[j]) {
-           tmp <- x [j] 
-           x[j] <- x[ j+ 1] 
-           x[j+1] <- tmp
-         }
-       }
-     }
-   }
-   else {
-     for(i in 1:(n-1)){
-       for(j in 1:(n-i)) {
-         if(x[j+1] > x[j]) {
-           tmp <- x [j] 
-           x[j] <- x[ j+ 1] 
-	   x[j+1] <- tmp
-	}
+bubble_sort <- function(x, ascending = TRUE) {
+  n <- length(x)
+  if (ascending) {
+    for (i in 1:(n - 1)) {
+      for (j in 1:(n - i)) {
+        if (x[j + 1] < x[j]) {
+          tmp <- x[j]
+          x[j] <- x[j + 1]
+          x[j + 1] <- tmp
+        }
+      }
+    }
+  }
+  else {
+    for (i in 1:(n - 1)) {
+      for (j in 1:(n - i)) {
+        if (x[j + 1] > x[j]) {
+          tmp <- x[j]
+          x[j] <- x[j + 1]
+          x[j + 1] <- tmp
+        }
       }
     }
   }
@@ -27,112 +27,122 @@ bubble_sort <- function (x,ascending = TRUE) {
 }
 
 #Merge sort
-mmerge<-function(a,b) {
-   r<-numeric(length(a)+length(b))
-   ai<-1; bi<-1; j<-1;
-   for(j in 1:length(r)) {
-     if((ai<=length(a) && a[ai]<b[bi]) || bi>length(b)) {
-       r[j] <- a[ai] 
-       ai <- ai+1
-     } else {
-       r[j] <- b[bi] 
-       bi <- bi+1
-     }
-   }
-   r
- }
+mmerge <- function(a, b) {
+  r <- numeric(length(a) + length(b))
+  ai <- 1;
+  bi <- 1;
+  j <- 1;
+  for (j in 1:length(r)) {
+    if ((ai <= length(a) && a[ai] < b[bi]) || bi > length(b)) {
+      r[j] <- a[ai]
+      ai <- ai + 1
+    } else {
+      r[j] <- b[bi]
+      bi <- bi + 1
+    }
+  }
+  r
+}
 
- mmergesort<-function(A) {
-   if(length(A)>1) {
-     q <- ceiling(length(A)/2)
-     a <- mmergesort(A[1:q])
-     b <- mmergesort(A[(q+1):length(A)])
-     mmerge(a,b)
-   } else {
-     A
-   }
- }
+mmergesort <- function(A) {
+  if (length(A) > 1) {
+    q <- ceiling(length(A) / 2)
+    a <- mmergesort(A[1:q])
+    b <- mmergesort(A[(q + 1):length(A)])
+    mmerge(a, b)
+  } else {
+    A
+  }
+}
 
 #Insertion sort
-insertion_sort <- function(A){
-   for (j in 2:length(A)) {
-     key = A[j] 
-     i = j - 1
-     while (i > 0 && A[i] > key) {
-       A[(i + 1)] = A[i] 
-       i = i - 1
-     }
-     A[(i + 1)] = key
-   }
-   A
- }
+insertion_sort <- function(A) {
+  for (j in 2:length(A)) {
+    key = A[j]
+    i = j - 1
+    while (i > 0 && A[i] > key) {
+      A[(i + 1)] = A[i]
+      i = i - 1
+    }
+    A[(i + 1)] = key
+  }
+  A
+}
 
 #Quicksort
- quickSort <- function(arr) {
-   # Pick a number at random.
-   mid <- sample(arr, 1)
+quickSort <- function(arr) {
+  # Pick a number at random.
+  mid <- sample(arr, 1)
 
-   # Place-holders for left and right values.
-   left <- c()
-   right <- c()
+  # Place-holders for left and right values.
+  left <- c()
+  right <- c()
 
-   # Move all the smaller values to the left, bigger values to the right.
-   lapply(arr[arr != mid], function(d) {
-     if (d < mid) {
-       left <<- c(left, d)
-     }
-     else {
-       right <<- c(right, d)
-     }
-   })
+  # Move all the smaller values to the left, bigger values to the right.
+  lapply(arr[arr != mid], function(d) {
+    if (d < mid) {
+      left <<- c(left, d)
+    }
+    else {
+      right <<- c(right, d)
+    }
+  })
 
-   if (length(left) > 1) {
-     left <- quickSort(left)
-   }
+  if (length(left) > 1) {
+    left <- quickSort(left)
+  }
 
-   if (length(right) > 1) {
-     right <- quickSort(right)
-   }
+  if (length(right) > 1) {
+    right <- quickSort(right)
+  }
 
-   # Finally, return the sorted values.
-   c(left, mid, right)
- }
+  # Finally, return the sorted values.
+  c(left, mid, right)
+}
 
 #Selection sort
-selection_sort <- function (x, ascending = TRUE) {
-   max <- length(x)
-   if (ascending) {
-     for (j in 1:(max-1)){
-       m <- x[j] 
-       p <- j
-       for(k in (j+1):max) {
-         if(x[k] < m) {
-           m <- x[k] 
-           p <- k
-         } ## end if
-       } ## end for k
-       x[p] <- x[j] 
-       x[j] <- m
-     } ## end for j
-   } ## end ascending if
-   else {
-     for (j in 1:(max-1)){
-       m <- x[j] 
-       p <- j
-       for(k in (j+1):max) {
-         if(x[k] > m) {
-           m <- x[k] 
-           p <- k
-         } ## end if
-       } ## end for k
-       x[p] <- x[j] 
-       x[j] <- m
-     } ## end for j
-   } ## end ascending else
-   x
- }
+selection_sort <- function(x, ascending = TRUE) {
+  max <- length(x)
+  if (ascending) {
+    for (j in 1:(max - 1)) {
+      m <- x[j]
+      p <- j
+      for (k in (j + 1):max) {
+        if (x[k] < m) {
+          m <- x[k]
+          p <- k
+        }
+        ## end if
+      }
+      ## end for k
+      x[p] <- x[j]
+      x[j] <- m
+    }
+    ## end for j
+  }
+  ## end ascending if
+  else {
+    for (j in 1:(max - 1)) {
+      m <- x[j]
+      p <- j
+      for (k in (j + 1):max) {
+        if (x[k] > m) {
+          m <- x[k]
+          p <- k
+        }
+        ## end if
+      }
+      ## end for k
+      x[p] <- x[j]
+      x[j] <- m
+    }
+    ## end for j
+  }
+  ## end ascending else
+  x
+}
 
-n <- 10**2
+n <- 10 ** 2
 
 bubbleTime <- numeric()
 selectionTime <- numeric()
@@ -140,7 +150,7 @@ quickSortTime <- numeric()
 mergeTime <- numeric()
 insertionTime <- numeric()
 
-for(i in 1:10){
+for (i in 1:10) {
   x <- runif(n)
   #Bubble Sort
   bubbleStart <- Sys.time()
@@ -179,19 +189,19 @@ for(i in 1:10){
 }
 
 t <- list("Bubble Sort" = bubbleTime,
-	  "Selection Sort" = selectionTime,
-	  "Quick Sort" = quickSortTime,
-	  "Merge sort" = mergeTime,
-	  "Insertion sort" = insertionTime)
+    "Selection Sort" = selectionTime,
+    "Quick Sort" = quickSortTime,
+    "Merge sort" = mergeTime,
+    "Insertion sort" = insertionTime)
 
-sort(sapply(t,mean))
-sort(sapply(t,sd))
+sort(sapply(t, mean))
+sort(sapply(t, sd))
 
-boxplot(t,main = "Tiempos de ejecucion de algoritmos",
+boxplot(t, main = "Tiempos de ejecucion de algoritmos",
           xlab = "Algoritmo",
-	        ylab = "Tiempo (s)", 
-          names = c("Bubble","Selection","Quick","Merge","Insertion"),
-          col = c("red","blue","green","yellow","purple")
+          ylab = "Tiempo (s)",
+          names = c("Bubble", "Selection", "Quick", "Merge", "Insertion"),
+          col = c("red", "blue", "green", "yellow", "purple")
           )
 
 # Crear una tabla
