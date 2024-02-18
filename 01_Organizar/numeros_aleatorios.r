fbeea901ce68b59ac9310535d981a3f647704bbf
Eri <103050349+Eriarer@users.noreply.github.com>
2024-02-18 14:09:02 -0600

    finall code working

--- 01_Organizar/numeros_aleatorios.r
+++ 01_Organizar/numeros_aleatorios.r
@@ -1,6 +1,8 @@
-n <- 10 ** 4
+exponencial <- 4
+base <- 10
+n <- base ** exponencial
 vueltas <- 100
-# Bubble Sort
+#Buble Sort
 bubble_sort <- function(x, ascending = TRUE) {
   n <- length(x)
   if (ascending) {
@@ -27,82 +29,7 @@ bubble_sort <- function(x, ascending = TRUE) {
   }
   x
 }
-
-#Merge sort
-mmerge <- function(a, b) {
-  r <- numeric(length(a) + length(b))
-  ai <- 1;
-  bi <- 1;
-  j <- 1;
-  for (j in 1:length(r)) {
-    if ((ai <= length(a) && a[ai] < b[bi]) || bi > length(b)) {
-      r[j] <- a[ai]
-      ai <- ai + 1
-    } else {
-      r[j] <- b[bi]
-      bi <- bi + 1
-    }
-  }
-  r
-}
-
-mmergesort <- function(A) {
-  if (length(A) > 1) {
-    q <- ceiling(length(A) / 2)
-    a <- mmergesort(A[1:q])
-    b <- mmergesort(A[(q + 1):length(A)])
-    mmerge(a, b)
-  } else {
-    A
-  }
-}
-
-#Insertion sort
-insertion_sort <- function(A) {
-  for (j in 2:length(A)) {
-    key = A[j]
-    i = j - 1
-    while (i > 0 && A[i] > key) {
-      A[(i + 1)] = A[i]
-      i = i - 1
-    }
-    A[(i + 1)] = key
-  }
-  A
-}
-
-#Quicksort
-quickSort <- function(arr) {
-  # Pick a number at random.
-  mid <- sample(arr, 1)
-
-  # Place-holders for left and right values.
-  left <- c()
-  right <- c()
-
-  # Move all the smaller values to the left, bigger values to the right.
-  lapply(arr[arr != mid], function(d) {
-    if (d < mid) {
-      left <<- c(left, d)
-    }
-    else {
-      right <<- c(right, d)
-    }
-  })
-
-  if (length(left) > 1) {
-    left <- quickSort(left)
-  }
-
-  if (length(right) > 1) {
-    right <- quickSort(right)
-  }
-
-  # Finally, return the sorted values.
-  c(left, mid, right)
-}
-
-#Selection sort
+#Selection Sort
 selection_sort <- function(x, ascending = TRUE) {
   max <- length(x)
   if (ascending) {
@@ -143,66 +70,126 @@ selection_sort <- function(x, ascending = TRUE) {
   ## end ascending else
   x
 }
+#Quick Sort
+# Quick sort algorithm:
+quickSort <- function(arr) {
+  # Pick a number at random.
+  mid <- sample(arr, 1)
 
-bubbleTime <- numeric()
-selectionTime <- numeric()
-quickSortTime <- numeric()
-mergeTime <- numeric()
-insertionTime <- numeric()
+  # Place-holders for left and right values.
+  left <- c()
+  right <- c()
+
+  # Move all the smaller values to the left, bigger values to the right.
+  lapply(arr[arr != mid], function(d) {
+    if (d < mid) {
+      left <<- c(left, d)
+    }
+    else {
+      right <<- c(right, d)
+    }
+  })
+
+  if (length(left) > 1) {
+    left <- quickSort(left)
+  }
+
+  if (length(right) > 1) {
+    right <- quickSort(right)
+  }
+
+  # Finally, return the sorted values.
+  c(left, mid, right)
+}
+#Merge Sort
+mmerge <- function(a, b) {
+  r <- numeric(length(a) + length(b))
+  ai <- 1;
+  bi <- 1;
+  j <- 1;
+  for (j in 1:length(r)) {
+    if ((ai <= length(a) && a[ai] < b[bi]) || bi > length(b)) {
+      r[j] <- a[ai]
+      ai <- ai + 1
+    } else {
+      r[j] <- b[bi]
+      bi <- bi + 1
+    }
+  }
+  r
+}
+
+mmergesort <- function(A) {
+  if (length(A) > 1) {
+    q <- ceiling(length(A) / 2)
+    a <- mmergesort(A[1:q])
+    b <- mmergesort(A[(q + 1):length(A)])
+    mmerge(a, b)
+  } else {
+    A
+  }
+}
+#Insertion Sort
+insertion_sort <- function(A) {
+  for (j in 2:length(A)) {
+    key = A[j]
+    i = j - 1
+    while (i > 0 && A[i] > key) {
+      A[(i + 1)] = A[i]
+      i = i - 1
+    }
+    A[(i + 1)] = key
+  }
+  A
+}
+
+tB <- numeric() #Creando vec para guardar los tiempos
+tS <- numeric()
+tQ <- numeric()
+tM <- numeric()
+tI <- numeric()
 
 for (i in 1:vueltas) {
   print(paste("Iteracion: ", i))
-  x <- runif(n)
+  x <- runif(n) #genera un vector de tamaño “n” (r= random, unif= uniforme)
 
-  #Bubble Sort
-  bubbleStart <- Sys.time()
-  bubble_res <- bubble_sort(x)
-  bubbleEnd <- Sys.time()
+  #métodos de ordenamiento
+  #Midiendo los tiempos de ordenamiento
 
-  bubbleTime <- c(bubbleTime, as.numeric(bubbleEnd - bubbleStart))
+  t0 <- Sys.time() #Valor inicial (tiempo de referencia)
 
-  #Selection Sort
-  selectionStart <- Sys.time()
-  selection_res <- selection_sort(x)
-  selectionEnd <- Sys.time()
+  y1 <- bubble_sort(x)
+  t1 <- Sys.time() #obtiene el tiempo del sistema
+  y2 <- insertion_sort(x)
+  t2 <- Sys.time()
+  y3 <- mmergesort(x)
+  t3 <- Sys.time()
+  y4 <- quickSort(x)
+  t4 <- Sys.time()
+  y5 <- selection_sort(x)
+  t5 <- Sys.time()
 
-  selectionTime <- c(selectionTime, as.numeric(selectionEnd - selectionStart))
-
-  #Quick Sort
-  quickSortStart <- Sys.time()
-  quickSort_res <- quickSort(x)
-  quickSortEnd <- Sys.time()
-
-  quickSortTime <- c(quickSortTime, as.numeric(quickSortEnd - quickSortStart))
-
-  #Merge sort
-  mergeStart <- Sys.time()
-  mergesort_res <- mmergesort(x)
-  mergeEnd <- Sys.time()
-
-  mergeTime <- c(mergeTime, as.numeric(mergeEnd - mergeStart))
-
-  #Insertion Sort
-  insertionStart <- Sys.time()
-  insertion_res <- insertion_sort(x)
-  insertionEnd <- Sys.time()
-
-  insertionTime <- c(insertionTime, as.numeric(insertionEnd - insertionStart))
+  #Calculando los tiempos de ejecución y concatencando los resultados
+  tB <- c(tB, as.numeric(t1 - t0))
+  tS <- c(tS, as.numeric(t2 - t1))
+  tQ <- c(tQ, as.numeric(t3 - t2))
+  tM <- c(tM, as.numeric(t4 - t3))
+  tI <- c(tI, as.numeric(t5 - t4))
 }
 
-t <- list("Bubble Sort" = bubbleTime,
-    "Selection Sort" = selectionTime,
-    "Quick Sort" = quickSortTime,
-    "Merge sort" = mergeTime,
-    "Insertion sort" = insertionTime)
+
+#Lista de los tiempos
+t <- list("Bubble" = tB,
+  "Selection" = tS,
+  "Quick" = tQ,
+  "Merge" = tM,
+  "Insertion" = tI)
 # Recrear la t con la lista 
 # ordenada por la media
-sapply(t, mean)
 
-boxplot(t, main = "Tiempos de ejecucion de algoritmos",
+boxplot(t, main = paste("Figura ", exponencial, "- 10^", exponencial),
           xlab = "Algoritmo",
           ylab = "Tiempo (s)",
-          names = c("Bubble", "Selection", "Quick", "Merge", "Insertion"),
           col = c("red", "blue", "green", "yellow", "purple")
           )
 
@@ -211,6 +198,6 @@ boxplot(t, main = "Tiempos de ejecucion de algoritmos",
 # ejeAlg    0.2 \u00B1 0.1
 table <- data.frame(
   Algoritmo = names(t),
-  Media = sapply(t, function(x) paste(mean(x), "±", sd(x)))
+  Media = sapply(t, function(x) paste(round(mean(x), 6), "±", round(sd(x), 6)))
 )
 table
